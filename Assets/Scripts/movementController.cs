80382fd2668e28d9b82ddf283aafba2f1e755995
Asante Azevedo <59629941+Croissantaye@users.noreply.github.com>
2021-02-06 00:49:26 -0800

    PlayerController, PlayerCharacter, CameraSystem
    Added basic player controller with jump, speed, and correct horizontal movement. Current player character is a place holder until the asset is made. Camera system added. Camera is locked onto player and will follow player. This works horizontally, but hasn't been tested vertically.

---
+++ Assets/Scripts/movementController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementController : MonoBehaviour
+{
+    public CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    public float playerSpeed = 2.0f;
+    public float jumpHeight = 1.0f;
+    public float gravityValue = -9.81f;
+    //public Transform cam;
+
+    //private void Start()
+    //{
+    //    controller = gameObject.AddComponent<CharacterController>();
+    //}
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3((Input.GetAxis("Horizontal")), 0, 0);
+        //move = cam.transform.right * move.x + cam.transform.forward * move.z;
+        //move.y = 0;
+        //move = move.normalized;
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
