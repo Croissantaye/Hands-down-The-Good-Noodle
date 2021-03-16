789b306a58bfc7d9feabca0702ab4b9937a3e657
Asante Azevedo <59629941+Croissantaye@users.noreply.github.com>
2021-03-15 21:54:13 -0700

    Checkpoints and Killzones
    Checkpoint prefab added. The check points can be placed in the level. When the player enter the trigger, the checkpoint stores the players position and makes that checkpoint the new checkpoint in the level manager. Checkpoints can only be activated once. Checkpoints also store players health and reset
    
    Killzone resets the player to the last checkpoint.

---
+++ Assets/Scripts/Mechanics/KillZone.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    public delegate void OnKillPlayer();
+    public static event OnKillPlayer Killed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        PlayerPlatformerController Player = other.GetComponent<PlayerPlatformerController>();
+        // Debug.Log("enter killzoe");
+        if(Player){
+            Debug.Log("kill player");
+            // Player.Die();
+            if(Killed != null){
+                Killed();
+            }
+        }
+    }
+}
