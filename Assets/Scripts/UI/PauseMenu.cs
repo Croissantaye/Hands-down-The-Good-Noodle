e1d304ee39f700d694ad0e775406e4b348c62d6a
Donovan <Dondo429@gmail.com>
2021-04-12 21:04:25 -0700

    Main Menu Ui/ Pause Menu Created

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool IsGamePaused = false;
+    public GameObject pauseMenuUI;
+    
+    
+    public void Resume()
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            IsGamePaused = false;
+        }
+
+        public void Pause()
+        {
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0;
+            IsGamePaused = true;
+        }
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsGamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+        
+    }
+}
